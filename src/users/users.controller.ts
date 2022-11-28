411b5435c5837a0f0f8b0f6348b913a5085b3f24
Rabindranath Ferreira <ferreiralox@gmail.com>
2022-11-28 14:21:48 +0100

    create users controllers base route and start calls

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
+const users = [
+  { id: '1', name: 'u1' },
+  { id: '2', name: 'u2' },
+];
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  async findAll() {
+    return users;
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: string) {
+    return users.find((user) => user.id === id);
+  }
+
+  @Post()
+  async createUser(@Body() createUserDTO: any) {
+    return createUserDTO;
+  }
+
+  @Put(':id')
+  async updateById(@Body() createUserDTO: any, @Param('id') id: string) {
+    return { id, createUserDTO };
+  }
+
+  @Delete(':id')
+  async deleteById(@Param('id') id: string) {
+    return { id };
+  }
+}
