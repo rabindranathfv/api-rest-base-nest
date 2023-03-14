a9933067ecf7322f83d4589f8fc1f31ad897e461
Rabindranath Ferreira <ferreiralox@gmail.com>
2023-03-14 10:14:28 +0100

    config: delete mongo from config and validation schemas

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -5,9 +5,9 @@ export const configuration = () => ({
     secret: process.env.JWT_SECRET,
     expiresIn: process.env.JWT_EXPIRES_IN,
   },
-  MONGO: {
-    uri: process.env.MONGO_URI,
-  },
+  // MONGO: {
+  //   uri: process.env.MONGO_URI,
+  // },
   CACHE: {
     ttl: process.env.NEST_TTL_CACHE,
     storage: process.env.NEST_MAX_CACHE_STORAGE,
--- src/config/env-schema.ts
+++ src/config/env-schema.ts
@@ -5,7 +5,7 @@ export const validationSchema = Joi.object({
   JWT_SECRET: Joi.string().required(),
   JWT_EXPIRES_IN: Joi.string().required(),
   PORT: Joi.number().default(3000),
-  MONGO_URI: Joi.string().required(),
+  // MONGO_URI: Joi.string().required(),
   NEST_TTL_CACHE: Joi.string().required(),
   NEST_MAX_CACHE_STORAGE: Joi.string().required(),
 });
